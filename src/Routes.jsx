88e67d625e10b56b8f848727621a5bed2cf22bec
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-04-27 21:41:39 -0300

    Public Route added

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -10,6 +10,25 @@ import App from './App';
 import Login from './pages/Login';
 import Panel from './pages/Panel';
 
+const PublicRoute = ({ component: Component, ...rest }) => {
+  const location = useLocation();
+
+  return (
+    <Route
+      {...rest}
+      render={(props) => (
+        isAuthenticated()
+          ? <Redirect to={{ pathname: '/painel', state: { from: location.pathname } }} />
+          : <Component {...props} />
+      )}
+    />
+  );
+};
+
+PublicRoute.propTypes = {
+  component: PropTypes.func.isRequired,
+};
+
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const location = useLocation();
 
@@ -33,8 +52,8 @@ const Routes = () => (
   <BrowserRouter>
     <App>
       <Switch>
-        <Route exact path="/" component={() => <Redirect to="/login" />} />
-        <Route path="/login" component={Login} />
+        <PublicRoute exact path="/" component={() => <Redirect to="/login" />} />
+        <PublicRoute path="/login" component={Login} />
         <PrivateRoute path="/painel/:category" component={Panel} />
         <PrivateRoute exact path="/painel" component={() => <Redirect to="/painel/Todos" />} />
       </Switch>
